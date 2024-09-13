456831213f49521804c7852e951b25073268e2f3
khyunwook <khyunwook0707@gmail.com>
2024-09-13 11:13:46 +0900

    feat :: userRepository 어노테이션 추가

--- src/main/java/com/memoir/domain/user/repository/UserRepository.java
+++ src/main/java/com/memoir/domain/user/repository/UserRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByAccountIdAndPassword(String AccountId, String Password);
