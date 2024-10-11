284f20ab406f9c56570cd95665eadeefe3350de5
khyunwook <khyunwook0707@gmail.com>
2024-10-11 11:40:43 +0900

    fix :: Request Valid 추가

--- src/main/java/com/memoir/domain/user/controller/dto/request/LoginRequest.java
+++ src/main/java/com/memoir/domain/user/controller/dto/request/LoginRequest.java
@@ -1,9 +1,14 @@
 package com.memoir.domain.user.controller.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class LoginRequest {
+
+    @NotNull
     private String accountId;
+
+    @NotNull
     private String password;
 }
