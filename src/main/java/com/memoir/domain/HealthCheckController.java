240bf4bb188000b0115b7028694e4410d9070e11
khyunwook <khyunwook0707@gmail.com>
2024-10-11 10:57:08 +0900

    feat :: health check 컨트롤러 추가

---
+++ src/main/java/com/memoir/domain/HealthCheckController.java
@@ -0,0 +1,11 @@
+package com.memoir.domain;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping
+    private String healthCheck() { return "OK"; }
+}
